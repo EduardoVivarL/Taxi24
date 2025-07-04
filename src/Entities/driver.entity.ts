4095448c939b5ccbd736730c6f12248e13bac4f1
EduardoVivarL <eduardo.vivar.l@outlook.com>
2025-07-03 22:30:46 -0600

    add ApiProperty tags to driver and passenger entities

--- src/Entities/driver.entity.ts
+++ src/Entities/driver.entity.ts
@@ -1,46 +1,61 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('Driver', { schema: 'dbo' })
 export class Driver {
+  @ApiProperty()
   @PrimaryGeneratedColumn({name: 'DriverID'})
   driverId: number;
 
+  @ApiProperty()
   @Column({name: 'FirstName'})
   firstName: string;
 
+  @ApiProperty()
   @Column({name: 'LastName'})
   lastName: string;
 
+  @ApiProperty()
   @Column({name: 'LicenseNumber'})
   licenseNumber: string;
 
+  @ApiProperty()
   @Column({name: 'NationalID'})
   nationalId: string;
 
+  @ApiProperty()
   @Column({name: 'Email'})
   email: string;
 
+  @ApiProperty()
   @Column({name: 'PhoneNumber'})
   phoneNumber: string;
 
+  @ApiProperty()
   @Column({name: 'IsActive'})
   isActive: Boolean;
 
+  @ApiProperty()
   @Column({name: 'IsOnline'})
   isOnline: string;
 
+  @ApiProperty()
   @Column({name: 'CatalogTravelStatusID'})
-  travelStatusId: string | null;
+  travelStatusId: string;
   
+  @ApiProperty()
   @Column({name: 'LatestTravelStatus'})
-  travelStatus: string | null;
+  travelStatus: string;
 
+  @ApiProperty()
   @Column({name: 'Latitude'})
   latitude: number;
 
+  @ApiProperty()
   @Column({name: 'Longitude'})
   longitude: number;
 
-@Column({name: 'LocationReportedDate'})
+  @ApiProperty()
+  @Column({name: 'LocationReportedDate'})
   locationReportedDate: Date;
 }
--- src/entities/passenger.entity.ts
+++ src/entities/passenger.entity.ts
@@ -1,22 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('Passenger', { schema: 'dbo' })
 export class Passenger {
+
+  @ApiProperty()
   @PrimaryGeneratedColumn({name: 'PassengerID'})
   passengerId: number;
 
+  @ApiProperty()
   @Column({name: 'FirstName'})
   firstName: string;
 
+  @ApiProperty()
   @Column({name: 'LastName'})
   lastName: string;
 
+  @ApiProperty()
   @Column({name: 'Email'})
   email: string;
 
+  @ApiProperty()
   @Column({name: 'PhoneNumber'})
   phoneNumber: string;
 
+  @ApiProperty()
   @Column({name: 'IsActive'})
   isActive: Boolean;
 }
